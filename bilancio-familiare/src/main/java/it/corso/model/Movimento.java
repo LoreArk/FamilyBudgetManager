e73039f9442c6a51c24fcdee1e4ba1462b12592d
Checco <francesco.zaza0510@gmail.com>
2025-05-05 20:44:56 +0200

    miao

--- bilancio-familiare/src/main/java/it/corso/model/Movimento.java
+++ bilancio-familiare/src/main/java/it/corso/model/Movimento.java
@@ -1,18 +1,26 @@
 package it.corso.model;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "bilancio")
 public class Movimento {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String descrizione;
-
     private LocalDateTime ricezione;
 
     @Column(name = "importo_entrata")
@@ -20,52 +28,4 @@ public class Movimento {
 
     @Column(name = "importo_uscita")
     private Integer importoUscita;
-
-    // Costruttore vuoto
-    public Movimento() {}
-
-    // Costruttore completo
-    public Movimento(String descrizione, LocalDateTime ricezione, Integer importoEntrata, Integer importoUscita) {
-        this.descrizione = descrizione;
-        this.ricezione = ricezione;
-        this.importoEntrata = importoEntrata;
-        this.importoUscita = importoUscita;
-    }
-
-    // Getter e Setter
-    public int getId() {
-        return id;
-    }
-
-    public String getDescrizione() {
-        return descrizione;
-    }
-
-    public void setDescrizione(String descrizione) {
-        this.descrizione = descrizione;
-    }
-
-    public LocalDateTime getRicezione() {
-        return ricezione;
-    }
-
-    public void setRicezione(LocalDateTime ricezione) {
-        this.ricezione = ricezione;
-    }
-
-    public Integer getImportoEntrata() {
-        return importoEntrata;
-    }
-
-    public void setImportoEntrata(Integer importoEntrata) {
-        this.importoEntrata = importoEntrata;
-    }
-
-    public Integer getImportoUscita() {
-        return importoUscita;
-    }
-
-    public void setImportoUscita(Integer importoUscita) {
-        this.importoUscita = importoUscita;
-    }
 }
