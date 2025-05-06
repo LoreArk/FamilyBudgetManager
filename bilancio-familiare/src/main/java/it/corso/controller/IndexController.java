910dc13a319ebd5e7a1edf764cf9565e7197e4c2
Gianmarco Minisi <gia.minisi@gmail.com>
2025-05-06 10:46:37 +0200

    Update IndexController.java

--- bilancio-familiare/src/main/java/it/corso/controller/IndexController.java
+++ bilancio-familiare/src/main/java/it/corso/controller/IndexController.java
@@ -1,20 +1,28 @@
 package it.corso.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.corso.model.Movimento;
+import it.corso.service.MovimentoService;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
-
+    
     @GetMapping
     public String renderPage(Model model){
-        Movimento movimento = new Movimento();
-        model.addAttribute("movimento", movimento);
+        
+        @Autowired
+        private MovimentoService movimentoService;
+        
+        List<Movimento> movimenti = movimentoService.getAllMovimenti();
+        model.addAttribute("movimenti", movimenti);
         return "index";
     }
 }
