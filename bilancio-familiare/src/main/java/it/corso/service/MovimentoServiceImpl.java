b0a69bb335163d7847b4175325d18975c1f394cc
AlinaBozhynska <bozhynska.alina@gmail.com>
2025-05-06 09:41:19 +0200

    ServiceImpl corretto

--- bilancio-familiare/src/main/java/it/corso/service/MovimentoServiceImpl.java
+++ bilancio-familiare/src/main/java/it/corso/service/MovimentoServiceImpl.java
@@ -1,10 +1,5 @@
 package it.corso.service;
 
-public interface MovimentoService {
-
-}
-package it.corso.service;
-
 import it.corso.model.Movimento;
 import it.corso.repository.MovimentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,35 +9,27 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class MovimentoService {
+public class MovimentoServiceImpl implements MovimentoService {
 
     @Autowired
     private MovimentoRepository movimentoRepository;
-
-    /**
-     * Restituisce tutti i movimenti presenti nel database.
-     */
+   
+        
     public List<Movimento> getAllMovimenti() {
-        return movimentoRepository.findAll();
+        return (List<Movimento>) movimentoRepository.findAll();
     }
-
-    /**
-     * Salva un nuovo movimento nel database.
-     */
+   
+      
     public void salvaMovimento(Movimento movimento) {
         movimentoRepository.save(movimento);
     }
 
-    /**
-     * Ritorna un movimento a partire dal suo ID, se esiste.
-     */
+       
     public Optional<Movimento> getMovimentoById(int id) {
         return movimentoRepository.findById(id);
     }
 
-    /**
-     * Elimina un movimento in base all'ID.
-     */
+         
     public void eliminaMovimento(int id) {
         movimentoRepository.deleteById(id);
     }
